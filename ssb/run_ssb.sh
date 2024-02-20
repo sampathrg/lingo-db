e3bbcf18c1cff0981d027a0fd0a0311d4e15afe2
Sampath Rajendra <sampathrg@gmail.com>
2024-02-20 11:12:16 +0000

    use num threads available on machine

--- ssb/run_ssb.sh
+++ ssb/run_ssb.sh
@@ -2,12 +2,12 @@
 
 # Set the LINGODB environment variables
 export LINGODB_EXECUTION_MODE=DEFAULT
-export LINGODB_PARALLELISM=32
+export LINGODB_PARALLELISM=`nproc` # Set num threads = num vcpus
 export QUERY_RUNS=5
 export LINGODB_PRINT_TYPE=csv
 
 file_names=("11" "12" "13" "21" "22" "23" "31" "32" "33" "34" "41" "42" "43")
-# file_names=("41" "42" "43")
+#file_names=("43")
 for file_name in "${file_names[@]}"; do
   echo "Running query: $file_name"
   $1/run-sql $2/$file_name.sql $3
