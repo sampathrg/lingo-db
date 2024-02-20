77fc89a5943aa06d0facb49e8fae2be0339c91eb
Sampath Rajendra <sampathrg@gmail.com>
2024-02-20 11:09:35 +0000

    Modify query to use dictencoding

--- resources/sql/ssb_de/34.sql
+++ resources/sql/ssb_de/34.sql
@@ -6,6 +6,6 @@ and lo_suppkey = s_suppkey
 and lo_orderdate = d_datekey
 and (c_city = 123 or c_city = 202) -- (c_city='UNITED KI1' or c_city='UNITED KI5') TODO - Check if this should be 203? 
 and (s_city = 114 or s_city = 191) -- (s_city='UNITED KI1' or s_city='UNITED KI5')
-and d_yearmonth = 'Dec1997'
+and d_yearmonth = 71 -- and d_yearmonth = 'Dec1997'
 group by c_city, s_city, d_year
 -- order by d_year asc, revenue desc;
